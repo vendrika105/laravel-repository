85503ec1e72bd57fc71a6eddfd06eac2127c6a8f
vendrika105 <vendrika105@gmail.com>
2025-02-11 14:03:56 +0700

    Add table name and connection name setter and getter

--- src/Repository.php
+++ src/Repository.php
@@ -4,5 +4,31 @@ namespace Vendrika105\LaravelRepository;
 
 class Repository
 {
+    protected string $table_name;
 
+    protected string $connection_name;
+
+    public function getTableName(): string
+    {
+        return $this->table_name;
+    }
+
+    public function setTableName(string $table_name): static
+    {
+        $this->table_name = $table_name;
+
+        return $this;
+    }
+
+    public function getConnectionName(): string
+    {
+        return $this->connection_name;
+    }
+
+    public function setConnectionName(string $connection_name): static
+    {
+        $this->connection_name = $connection_name;
+
+        return $this;
+    }
 }
